0b77ff8f5e696aa600c4dfe5c3a1ab1a4be103fc
Ananya Jain <ananyaj1515@gmail.com>
2026-03-07 13:15:15 +0800

    Add new tag command

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,5 +11,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_TAG_YEAR = new Prefix("y=");
+    public static final Prefix PREFIX_TAG_MAJOR = new Prefix("m=");
+    public static final Prefix PREFIX_TAG_GENDER = new Prefix("g=");
 
 }
